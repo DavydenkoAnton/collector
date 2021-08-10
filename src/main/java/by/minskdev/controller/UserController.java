a255e83552cdaf24003f3c8bdd03fe39ed30dee7
DavydenkoAnton <davydenko83@gmail.com>
2021-08-10 23:56:57 +0300

    add userEdit.html

--- src/main/java/by/minskdev/controller/UserController.java
+++ src/main/java/by/minskdev/controller/UserController.java
@@ -1,9 +1,13 @@
 package by.minskdev.controller;
 
+import by.minskdev.entity.Role;
+import by.minskdev.entity.User;
 import by.minskdev.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,7 +21,16 @@ public class UserController {
     }
 
     @GetMapping
-    public String userList() {
+    public String userList(Model model) {
+        model.addAttribute("users", userRepository.findAll());
         return "userList";
+    }
+
+    @GetMapping("{userId}")
+    public String userEditForm(@PathVariable Integer userId, Model model) {
+        User user = userRepository.findUserById(userId);
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
     }
 }
--- src/main/java/by/minskdev/repositories/UserRepository.java
+++ src/main/java/by/minskdev/repositories/UserRepository.java
@@ -4,6 +4,10 @@ package by.minskdev.repositories;
 import by.minskdev.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    User findUserById(Integer id);
 }
